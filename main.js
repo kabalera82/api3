a80dff4f1decc2cd9c345db11640b0eecc59b1a3
kabalera82 <152176929+kabalera82@users.noreply.github.com>
2024-09-09 21:11:10 +0200

    fin

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ const cogePelis = async () => {
         japaneseTitle: item.original_title,
         romanTitle: item.original_title_romanised,
         poster: item.image,
+        description: item.description,
+        director: item.director,
+        productor: item.producer,
+        releaseDate: item.release_date,
+        puntuacion: item.rt_score,
         id: item.id,
+        banner:item.movie_banner,
+
     }));
     imprimePelis(peliculas);
 };
@@ -17,10 +24,36 @@ const imprimePelis = (movies) => {
         li.innerHTML = `
         <h2>${movie.englishTitle}</h2>
         <h3>${movie.japaneseTitle}</h3>
-        <h4>${movie.romanTitle}
+        <h4>${movie.romanTitle}</h4>
         <img src="${movie.poster}" alt="${movie.englishTitle} poster"/>
         `;
+
+        const divInfo = document.createElement("div");
+        divInfo.classList.add("divInfo");
+        divInfo.innerHTML = `
+            <h3>${movie.englishTitle}</h3>
+            <h4>${movie.japaneseTitle}</h4>
+            <p>${movie.romanTitle}</p>
+            <p>${movie.description}</p>
+            <p>${movie.director}</p>
+            <p>${movie.productor}</p>
+            <p>${movie.releaseDate}</p>
+            <p>People Rate: ${movie.puntuacion}</p>
+            <img src="${movie.banner}" alt="${movie.japaneseTitle} banner"/>
+            
+        `;
+        divInfo.style.display = 'none'; // Ocultar por defecto
+        
+        li.appendChild(divInfo);
         document.querySelector("#movies").appendChild(li);
+        
+        li.addEventListener('click', () => {
+            if (divInfo.style.display === 'none') {
+                divInfo.style.display = 'block';
+            } else {
+                divInfo.style.display = 'none';
+            }
+        });
     }
 };
 
